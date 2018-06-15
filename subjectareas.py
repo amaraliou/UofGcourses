e1185c38ba96bc93be027f50816042e6134272b4
amaraliou <amarraliou97@gmail.com>
2018-06-15 13:48:15 +0100

    Adding subject areas

---
+++ subjectareas.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+
+mainurl = 'https://www.gla.ac.uk'
+subjectsurl = mainurl + '/coursecatalogue/browsebysubjectarea/'
+
+r = requests.get(subjectsurl)
+page = r.text
+soup = BeautifulSoup(page, 'html.parser')
+
+subjectmain = soup.find('div', attrs={'class': 'maincontent'})
+subs_html = subjectmain.find_all('a')
+
+subjectareas = {}
+
+for subject in subs_html:
+    code = re.findall('[A-Z]+', str(subject.attrs['href']))[0]
+    subjectareas[code] = {'title': subject.attrs['title'], 'url': mainurl + subject.attrs['href']}
+
+for k, v in subjectareas.items():
+    print(k, v)
+
+f = open('data/subjects.json', 'w+')
+json.dump(subjectareas, f)
